ce85982429f938fc274195a2e279e18f9a1f4381
Akhil Narang <me@akhilnarang.dev>
2024-03-23 22:37:30 +0530

    feat: delete the files parallely
    Probably the bad idea the way its implemented - should limit the concurrency
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 	"time"
 
 	"google.golang.org/api/drive/v3"
@@ -54,12 +55,26 @@ func main() {
 		log.Fatalf("Failed to list files: %v", err)
 	}
 
+	// Initialize a waitgroup
+	var wg sync.WaitGroup
+
 	// Iterate over and delete the files
 	for _, file := range files.Files {
 		fmt.Printf("Deleting %s\n", file.Name)
-		err = client.Files.Delete(file.Id).SupportsAllDrives(true).Do()
-		if err != nil {
-			log.Fatalf("Failed to delete file: %v", err)
-		}
+		wg.Add(1)
+		go deleteFile(client, file.Id, &wg)
+	}
+
+	fmt.Println("Waiting for all goroutines to finish")
+	wg.Wait()
+	fmt.Println("All files deleted successfully")
+}
+
+// deleteFile deletes the file with the given ID from the connected drive
+func deleteFile(client *drive.Service, fileID string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	err := client.Files.Delete(fileID).SupportsAllDrives(true).Do()
+	if err != nil {
+		log.Fatalf("Failed to delete file: %v", err)
 	}
 }
